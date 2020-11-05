d0de9b1552bac79074fb2e402708ec595d26528e
Rangel Reale <rangelspam@gmail.com>
2020-11-05 15:44:58 -0300

    release 0.7.4 * Fix KDataHelper handling

--- kg_rabbitmq/__init__.py
+++ kg_rabbitmq/__init__.py
@@ -9,7 +9,7 @@ from .configfile import (
     RabbitMQConfigFile,
 )
 
-__version__ = "0.7.3"
+__version__ = "0.7.4"
 
 __all__ = [
     'RabbitMQOptions',
--- kg_rabbitmq/builder.py
+++ kg_rabbitmq/builder.py
@@ -426,7 +426,7 @@ class RabbitMQBuilder(Builder):
                             },
                             KDataHelper_Volume.info(base_value={
                                 'name': 'rabbitmq-config-erlang-cookie',
-                            }, kdata=self.option_get('config.erlang_cookie'), default_value={
+                            }, value=self.option_get('config.erlang_cookie'), default_value={
                                 'secret': {
                                     'secretName': self.object_name('config-secret'),
                                     'items': [{
@@ -437,7 +437,7 @@ class RabbitMQBuilder(Builder):
                             }, key_path='erlang_cookie'),
                             KDataHelper_Volume.info(base_value={
                                 'name': 'rabbitmq-config-load-definition',
-                            }, kdata=self.option_get('config.load_definitions'), default_value={
+                            }, kdata_value=self.option_get('config.load_definitions'), default_value={
                                 'secret': {
                                     'secretName': self.object_name('config-secret'),
                                     'items': [{
@@ -449,7 +449,7 @@ class RabbitMQBuilder(Builder):
                                 enabled=self.option_get('config.load_definitions') is not None),
                             KDataHelper_Volume.info(base_value={
                                 'name': 'rabbitmq-data',
-                            }, kdata=self.option_get('kubernetes.volumes.data')),
+                            }, value=self.option_get('kubernetes.volumes.data')),
                         ],
                         'serviceAccountName': ValueData(value=self.object_name('service-account'),
                                                         enabled=self.object_name('service-account') is not None),
